11872d01d0a778a58234eef63fbf3ff26a79ba31
ujjwal9019 <ujjwal9019@gmail.com>
2024-02-28 10:50:56 +0530

    Redux Toolkit Configure

--- blogs/src/App.jsx
+++ blogs/src/App.jsx
@@ -1,13 +1,41 @@
-
-
+import { useState  , useEffect} from "react"
+import { useDispatch } from 'react-redux'
+import authService from "./appwrite/auth"
+import { login , logout } from "./store/authSlice"
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
 function App() {
 
+  const [loading , setLoading] = useState(true)
 
-  return (
-    <>
-      <h1>hii</h1>
-    </>
-  )
+  const  dispatch = useDispatch()
+
+
+  // use eefect work in this is to ask if user is login or not
+  
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+
+// conditional rendering
+return !loading ? (
+  <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="w-full block">
+      <Header/>
+      {/* <Outlet/> */}
+      <Footer/>  
+    </div>
+  </div>
+) :(null) 
 }
 
 export default App
---
+++ blogs/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/footer";
+
+export {
+    Header,
+    Footer
+}
